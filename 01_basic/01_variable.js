5f9bdc80240da1a013297d5318f655078847e994
himanshughorai07 <himanshughoraiyt07@gmail.com>
2024-11-27 07:46:45 +0000

    console table add

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,1 +1,17 @@
-console.log("this is demo file");
+// console.log("this is demo file");
+
+const accountId = 12332224;
+let accountEmail = "himanshughorai.demo@gmail.com";
+var accountPassword = "0938664";
+
+accountCity = "kolkata";
+// accountId = 6436754854;
+accountEmail ="demo@gmail.com"
+
+// accountId = 2 ;  this is not allowd in java script ;
+
+// console.log("this is string");
+// console.log(accountPassword);
+
+
+console.table([accountCity,accountId,accountEmail]);
